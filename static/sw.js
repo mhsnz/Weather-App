9f211247092bf8c27edf1c8f19dae61340aba1ee
mhsnz <mhsnkarimi468@gmail.com>
2025-02-10 00:46:28 +0330

    Create sw.js

---
+++ static/sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'weather-app-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
